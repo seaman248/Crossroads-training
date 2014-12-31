d6c6a48ee03898dce6e5463de13e4ea9845b177e
seaman <semen.bko@ya.ru>
2014-12-31 16:12:31 +0300

    lesson 2

--- main2.js
+++ main2.js
@@ -1,6 +1,13 @@
 var http = require('http'),
 	crossroads = require('crossroads');
 
+var spitsRoute = crossroads.addRoute('spits/:id:');
+
+spitsRoute.matched.add(function(id){
+		console.log(id);
+	});
+
 http.createServer(function(req, res){
-	
+	crossroads.parse(req.url);
+	res.end('asdf');
 }).listen(8080);	
